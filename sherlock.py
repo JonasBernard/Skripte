28dae9e9cd8ec9818f3cabe0c10711e36c7eae63
Jonas <pass.jbernard@web.de>
2020-07-10 14:22:12 +0200

    initial commit

---
+++ sherlock.py
@@ -0,0 +1,21 @@
+quadratzahlen = []
+
+
+def quadratzahlen_generieren(von, bis):
+	for i in range(von, bis):
+		quadratzahlen.append(i ** 2)
+
+
+def sherlock(x):
+	if x/2 + 1 in quadratzahlen and x + 1 in quadratzahlen:
+		return True
+	return False
+
+
+quadratzahlen_generieren(25, 50)
+for i in quadratzahlen:
+	print("Überprüfe {0}".format(i - 1))
+	if sherlock(i - 1):
+		print("Bedingung erfüllt für " + str(i - 1))
+	else:
+		print("Bedingung nicht erfüllt für " + str(i - 1))
